1408542a70d06a76cde1b7fdf24a0effc23e2839
Victor <vb4007@gmail.com>
2018-06-17 22:17:48 +0200

    Switch to boost threads

--- TubeNetwork/TubeNetwork.cpp
+++ TubeNetwork/TubeNetwork.cpp
@@ -1,14 +1,16 @@
 ﻿#include <iostream>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
 
 #include "network/Server.h"
 #include "network/Client.h"
 
 int main(int argc, char* argv[])
 {
-	std::thread runServer(Server::serve);
-	std::thread runClient(Client::run);
+	boost::thread runServer{ Server::serve };
+	
+	Client::run();
 
 	return 0;
 }
